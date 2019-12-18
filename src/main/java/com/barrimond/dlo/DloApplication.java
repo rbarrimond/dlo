77cb2a71bf8bda47a7b287aa2b5b0a99e80e6e46
Robert Barrimond <robert@barrimond.com>
2019-12-18 00:48:22 -0500

    project cleanup

--- src/main/java/com/barrimond/dlo/DloApplication.java
+++ src/main/java/com/barrimond/dlo/DloApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DloApplication.class, args);
 	}
 
 }
